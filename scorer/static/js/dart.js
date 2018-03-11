114f1e2fd052a0a5368225d1575e79ac6a80aabd
Noah Hart <nhart624@gmail.com>
2018-03-11 12:40:27 -0500

    fixed calculator scoring (#1)

--- scorer/static/js/dart.js
+++ scorer/static/js/dart.js
@@ -1,20 +1,44 @@
 $(document).ready(function() {
+    var throwFocus = 1;
+    var $throwFocus = $("#throw1");
+
     console.log("hello");
-    $("#throw1").val('0');
-    $("#throw2").val('0');
-    $("#throw3").val('0');
-    var throw = 1;
+//    $("#throw1").val('0');
+//    $("#throw2").val('0');
+//    $("#throw3").val('0');
 
     $(".num_input").on("click", function() {
-        $throw1 = $(this);
-        $throw1.value += $(this);
-        $this.css("background-color", "blue");
-        console.log("jjuy");
-        console.log($this);
+        var $this = $(this);
+        console.log("$this:", $this[0]);
+        console.log("$this.value:", $this[0].value);
+
+        var numButtonClickedVal = $this[0].value;
+        var currentVal = $throwFocus.val();
+        var newVal = currentVal + numButtonClickedVal;
+        $throwFocus.val(newVal);
+//        $(this).css("background-color", "blue");
     });
 
-    function next(){
-    var throw = throw++;
-    console.log(throw);
-    )
+    function update_throw_state() {
+      console.log("next throw");
+      if (throwFocus === 1) {
+        throwFocus = 2;
+        $throwFocus =  $("#throw2");
+      } else if (throwFocus === 2) {
+        throwFocus = 3;
+        $throwFocus =  $("#throw3");
+      } else {
+        throwFocus = 1;
+        $throwFocus =  $("#throw1");
+      }
+    }
+
+    function foo() {
+        console.log('foo');
+    }
+
+    $("#next-throw").on('click', update_throw_state);
+    $("#next-throw").on('click', function(){console.log('NEXT');});
+
+
 });
